74b9b3699e56aa976fa123f5f0798d56c16181fe
Long Pham <longpham@sandiego.edu>
2024-11-21 15:42:49 -0800

    Edited text

--- smse_onboarding/tasks/views.py
+++ smse_onboarding/tasks/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the dashboard.")
+    return HttpResponse("Hello world. You're at the dashboard.")
