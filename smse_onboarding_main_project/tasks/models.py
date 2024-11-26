1c1d190716e8b2154f92d84726d4578a32ff31d8
Long Pham <longpham@sandiego.edu>
2024-11-25 18:55:15 -0800

    Changed the name of the main folder and added task model

---
+++ smse_onboarding_main_project/tasks/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+    deadline = models.DateTimeField()
+
+    class Meta:
+        ordering = ['created_at']
