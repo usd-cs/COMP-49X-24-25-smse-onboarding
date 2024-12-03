90ffb537366d14639ecf40d7a361b492f2202938
Cooor12 <nickzhou@sandiego.edu>
2024-12-02 20:52:09 -0800

    further fixing formatting for flake8

--- smse_onboarding_main_project/tasks/views.py
+++ smse_onboarding_main_project/tasks/views.py
@@ -1,13 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render, redirect
-from django.http import HttpResponseForbidden
-from django.http import JsonResponse
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
 from .models import Task
 
 
 def home(request):
-    #return HttpResponse("Hello world. You're at the dashboard.")
+    # return HttpResponse("Hello world. You're at the dashboard.")
     # Task.objects.create(
     #     title = "Get Zoom phone",
     #     description = "Get a Zoom phone number.",
@@ -18,6 +15,7 @@ def home(request):
     tasks = Task.objects.all()
     return render(request, 'new_hire_dashboard/home.html', {"tasks": tasks})
 
+
 def notifications(request):
-    #return HttpResponse("Hello world. You're at the dashboard.")
+    # return HttpResponse("Hello world. You're at the dashboard.")
     return render(request, 'new_hire_dashboard/notifications.html')
