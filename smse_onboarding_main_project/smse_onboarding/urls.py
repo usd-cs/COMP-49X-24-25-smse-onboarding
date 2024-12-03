d8e7157bc37b9d472e2815560d9befc090bd5abe
Long Pham <longpham@sandiego.edu>
2024-12-02 18:12:22 -0800

    Completed tasks tables

--- smse_onboarding_main_project/smse_onboarding/urls.py
+++ smse_onboarding_main_project/smse_onboarding/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", include("tasks.urls")),
--- smse_onboarding_main_project/tasks/views.py
+++ smse_onboarding_main_project/tasks/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
+from django.http import HttpResponseForbidden
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from .models import Task
 
 
 def home(request):
     #return HttpResponse("Hello world. You're at the dashboard.")
-    return render(request, 'new_hire_dashboard/home.html')
+    # Task.objects.create(
+    #     title = "Get Zoom phone",
+    #     description = "Get a Zoom phone number.",
+    #     created_at = "2024-12-02 18:07",
+    #     completed = True,
+    #     deadline = "2025-01-05 23:59",
+    # )
+    tasks = Task.objects.all()
+    return render(request, 'new_hire_dashboard/home.html', {"tasks": tasks})
+
+def notifications(request):
+    #return HttpResponse("Hello world. You're at the dashboard.")
+    return render(request, 'new_hire_dashboard/notifications.html')
