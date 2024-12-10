b8a0c5462ed07128c7782bba4e4d892dd03e6f24
scnbello <sbello@sandiego.edu>
2024-12-09 23:41:20 -0800

    tasks layout fixed and attempt at making styles.css work

--- smse_onboarding_main_project/smse_onboarding/settings.py
+++ smse_onboarding_main_project/smse_onboarding/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +78,11 @@ WSGI_APPLICATION = 'smse_onboarding.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'smse-db',       
-        'USER': 'postgres',      
-        'PASSWORD': 'password',  
-        'HOST': 'localhost',     
-        'PORT': '5432',          
+        'NAME': 'smse-db',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -121,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'tasks', 'static'),  # Correct path to tasks/static
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
