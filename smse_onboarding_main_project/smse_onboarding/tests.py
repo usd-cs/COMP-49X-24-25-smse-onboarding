48c8d7c33a2b4a265783d21c4771864aa8fa8f72
Long Pham <longpham@sandiego.edu>
2024-12-10 14:07:54 -0800

    added docstrings

--- smse_onboarding_main_project/smse_onboarding/tests.py
+++ smse_onboarding_main_project/smse_onboarding/tests.py
@@ -6,6 +6,15 @@ from datetime import datetime, timedelta
 class TaskMock:
     """Mock for Task model."""
     def __init__(self, task_id, task_name, due_date, completed_status):
+        """
+        Function for setting up the mock for the Task model.
+
+        Args:
+            task_id: The ID for the task.
+            task_name: The name for the task.
+            due_date: The due date for the task.
+            completed_status: The completion status of the task.
+        """
         self.task_id = task_id
         self.task_name = task_name
         self.due_date = due_date
@@ -16,6 +25,15 @@ class TaskMock:
 class TaskProgressMock:
     """Mock for TaskProgress model."""
     def __init__(self, progress_id, task, faculty, progress_status):
+        """
+        Function for setting up the mock for the TaskProgress model.
+
+        Args:
+            progress_id: The ID for the task progress.
+            task: The name for the task.
+            faculty: The faculty member that the task is assigned to.
+            progress_status: The progress and completion status of the task.
+        """
         self.progress_id = progress_id
         self.task = task
         self.faculty = faculty
@@ -24,9 +42,15 @@ class TaskProgressMock:
 
 
 class TaskTests(TestCase):
-    """Unit tests for Task model with a mocked database."""
+    """
+    Unit tests for Task model with a mocked database.
+
+    Args:
+        TestCase: Inherits from the TestCase class.
+    """
 
     def setUp(self):
+        """Set up the mock for the task."""
         # Mock a task instance
         self.mock_task = TaskMock(
             task_id=1,
@@ -50,9 +74,15 @@ class TaskTests(TestCase):
 
 
 class TaskProgressTests(TestCase):
-    """Unit tests for TaskProgress model with a mocked database."""
+    """
+    Unit tests for TaskProgress model with a mocked database.
+    
+    Args:
+        TestCase: Inherits from the TestCase class.
+    """
 
     def setUp(self):
+        """Set up the mock for the task's progress."""
         # Mock faculty and task instances
         self.mock_task = TaskMock(
             task_id=1,
