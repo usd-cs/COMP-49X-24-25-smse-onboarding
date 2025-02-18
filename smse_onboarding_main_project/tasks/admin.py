2c654bcbffc9b10df654e22be6abe09815c15a24
scnbello <sbello@sandiego.edu>
2025-02-18 14:56:12 -0800

    changed admin.py

--- smse_onboarding_main_project/tasks/admin.py
+++ smse_onboarding_main_project/tasks/admin.py
@@ -3,8 +3,24 @@ Admin file.
 """
 from django.contrib import admin
 
-from .models import Task
+from .models import Task, Faculty
 
 # Register models.
-admin.site.register(Task)
+#admin.site.register(Task)
 
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'deadline')
+    search_fields = ('title', 'deadline')
+
+@admin.register(Faculty)
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'email')
+    list_filter = ('first_name',)
+
+    def assigned_tasks(self, obj):
+        return ", ".join([task.title for task in obj.tasks.all()])
+    assigned_tasks.short_description = "Assigned Tasks"
+
+    list_display += ('assigned_tasks',)
