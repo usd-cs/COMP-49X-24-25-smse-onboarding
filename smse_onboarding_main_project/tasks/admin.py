ad49f6cf2a857a196bd8158d0b1d05d6e51cf623
scnbello <sbello@sandiego.edu>
2025-02-19 10:41:48 -0800

    added docstrings to classes and functions

--- smse_onboarding_main_project/tasks/admin.py
+++ smse_onboarding_main_project/tasks/admin.py
@@ -9,17 +9,39 @@ from .models import Task, Faculty, TaskProgress, FacultyDocument
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
+    """
+    Customizes the Django Admin panel for managing tasks.
+    It shows task details, allows filtering and searching, and
+    shows tasks assignments. 
+
+    """
     list_display = ('title', 'assigned_to', 'completed', 'deadline')
     search_fields = ('title', 'assigned_to__first_name', 'assigned_to__last_name')
     list_filter = ('completed',)
 
 @admin.register(Faculty)
 class FacultyAdmin(admin.ModelAdmin):
+
+    """
+    This class customizes the Django Admin panel for managing faculty members.
+    It categorizes faculty based on their onboarding status and user role and
+    allows search and filtering.
+    """
     list_display = ('first_name', 'last_name', 'engineering_dept', 'email', 'completed_onboarding', 'user_type')
     search_fields = ('first_name', 'last_name', 'email')
     list_filter = ('engineering_dept', 'completed_onboarding')
 
     def user_type(self, obj):
+        """
+        Determines the role of a faculty member based on user permissions and onboarding status.
+
+        Args:
+            obj (Faculty): The faculty.
+
+        Returns:
+            str: Represents the user's role.
+        """
+
         if obj.user.is_superuser:
             return "Superuser Admin"
         elif obj.user.is_staff:
@@ -33,5 +55,9 @@ class FacultyAdmin(admin.ModelAdmin):
 
 @admin.register(FacultyDocument)
 class FacultyDocumentAdmin(admin.ModelAdmin):
+    """
+    This class customizes the Django Admin panel for managing faculty documents.
+    It allows searching and displaying faculty document information.
+    """
     list_display = ('faculty', 'file_path')
     search_fields = ('faculty__first_name', 'faculty__last_name')
