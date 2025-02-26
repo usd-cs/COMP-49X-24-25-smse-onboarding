f1cabb86c9b624fe8dc9919ba23e6bc3564a26e1
scnbello <sbello@sandiego.edu>
2025-02-26 00:15:18 -0800

    include media file config in settings

--- smse_onboarding_main_project/smse_onboarding/settings.py
+++ smse_onboarding_main_project/smse_onboarding/settings.py
@@ -135,3 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/home/'
 LOGOUT_REDIRECT_URL = '/login/'
 LOGIN_URL = '/login/'
+
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
