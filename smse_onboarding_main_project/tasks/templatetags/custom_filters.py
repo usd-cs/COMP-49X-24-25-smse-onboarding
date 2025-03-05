0c0dec1b3b7022eeb223ff65ef9a79b68841b451
scnbello <sbello@sandiego.edu>
2025-03-04 22:30:42 -0800

    separated  tables from dashboard

--- smse_onboarding_main_project/tasks/templatetags/custom_filters.py
+++ smse_onboarding_main_project/tasks/templatetags/custom_filters.py
@@ -5,3 +5,11 @@ register = template.Library()
 @register.filter
 def ends_with(value, arg):
     return value.endswith(arg)
+
+@register.filter
+def absolute(value):
+    """Returns the absolute value"""
+    try:
+        return abs(int(value))
+    except (ValueError, TypeError):
+        return value
