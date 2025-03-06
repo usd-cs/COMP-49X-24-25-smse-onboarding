fcd13ba31d421b4ea4cc1d9992d98b215568bfd3
Cooor12 <nickzhou@sandiego.edu>
2025-03-05 20:20:07 -0800

    adding back to admin dashboard button in help guide and admin page

--- smse_onboarding_main_project/smse_onboarding/settings.py
+++ smse_onboarding_main_project/smse_onboarding/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'smse_onboarding.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'tasks', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
