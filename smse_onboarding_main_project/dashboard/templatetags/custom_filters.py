8ff10071d8dcee9355ba83d38929f4e0e3ffdc50
scnbello <sbello@sandiego.edu>
2025-03-21 13:23:30 -0700

    fix  default no tasks message in tables

--- smse_onboarding_main_project/dashboard/templatetags/custom_filters.py
+++ smse_onboarding_main_project/dashboard/templatetags/custom_filters.py
@@ -11,3 +11,8 @@ def absolute(value):
 def completed_tasks(tasks):
     """Return only completed tasks"""
     return [task for task in tasks if task.is_completed_by_faculty]
+
+@register.filter
+def upcoming_tasks(tasks):
+    """Return only upcoming (not completed) tasks"""
+    return [task for task in tasks if not task.is_completed_by_faculty]
