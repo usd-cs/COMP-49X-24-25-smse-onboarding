214214e228a4e9336b3b8e791acf93acca0b76df
scnbello <sbello@sandiego.edu>
2025-03-21 19:56:36 -0700

    fixed issue with branch and lost documents

--- smse_onboarding_main_project/documents/urls.py
+++ smse_onboarding_main_project/documents/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = 'documents'
 
 urlpatterns = [
-    path('', views.show_documents, name='list'),
-    path('upload/', views.upload_document, name='upload'),
-    path('download/<int:doc_id>/', views.download_document, name='download'),
-    path('delete/<int:doc_id>/', views.delete_document, name='delete'),
+    path('', views.show_documents, name='show_documents'),
+    path('faculty/<int:faculty_id>/', views.show_documents, name='faculty_documents'),
+    path('upload/', views.upload_document, name='upload_document'),
+    path('delete/<int:doc_id>/', views.delete_document, name='delete_document'),
+    path('download/<int:doc_id>/', views.download_document, name='download_document'),
 ]
--- smse_onboarding_main_project/smse_onboarding/settings.py
+++ smse_onboarding_main_project/smse_onboarding/settings.py
@@ -159,9 +159,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'tasks', 'static'),  # Correct path to tasks/static
+    BASE_DIR / 'dashboard' / 'static',
 ]
 
 # Default primary key field type
