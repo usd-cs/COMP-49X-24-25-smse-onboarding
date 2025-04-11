e68884a5c2517e4b8f080d1e9ded914241775afe
Long Pham <longpham@sandiego.edu>
2025-04-11 14:57:01 -0700

    Further modifications

--- smse_onboarding_main_project/reminders/views.py
+++ smse_onboarding_main_project/reminders/views.py
@@ -19,12 +19,15 @@ def send_reminder(request, faculty_id, current_task_id):
             days_remaining = "This task is overdue."
             time_remaining = "Overdue"
 
-        first_three_sentences = f"This is a reminder about your new hire onboarding task: {current_task.title}. This task is due on {current_task.deadline.strftime('%B %d, %Y, %I:%M %p')}. {days_remaining}"
-        next_two_sentences = f"Please complete this task as soon as possible. Please log in to the SMSE Onboarding Portal at https://smse-onboarding.dedyn.io to view the task and complete it."
+        first_sentence = f"This is a reminder about your new hire onboarding task: {current_task.title}."
+        second_sentence = f"This task is due on {current_task.deadline.strftime('%B %d, %Y, %I:%M %p')}."
+        third_sentence = f"{days_remaining}"
+        fourth_sentence = f"Please complete this task as soon as possible."
+        fifth_sentence = f"Please log in to the SMSE Onboarding Portal at https://smse-onboarding.dedyn.io to view the task and complete it."
 
         message = f"""Hello {faculty.first_name} {faculty.last_name},
 
-{first_three_sentences} {next_two_sentences}
+{first_sentence} {second_sentence} {third_sentence} {fourth_sentence} {fifth_sentence}
 
 See below for the task details:
 
