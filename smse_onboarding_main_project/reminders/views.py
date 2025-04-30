90cc5bbc7edd02dea639d161f4ca1fd81d0d5b8c
Long Pham <longpham@sandiego.edu>
2025-04-30 04:15:52 -0700

    finished notifications page

--- smse_onboarding_main_project/reminders/views.py
+++ smse_onboarding_main_project/reminders/views.py
@@ -59,6 +59,8 @@ def send_reminder_faculty(request, faculty_id, current_task_id):
             days_remaining = "This task is overdue."
             time_remaining = "Overdue"
 
+        send_notification_faculty(faculty, current_task, days_remaining, time_remaining)
+
         first_sentence = f"This is a reminder about your new hire onboarding task: {current_task.title}."
         second_sentence = f"This task is due on {current_task.deadline.strftime('%B %d, %Y, %I:%M %p')}."
         third_sentence = f"{days_remaining}"
@@ -84,15 +86,16 @@ Thank you,
 
 SMSE Admin Team"""
         
-        send_mail(
-            f"Reminder for New Hire Onboarding Task: {current_task.title}",
-            message,
-            settings.EMAIL_HOST_USER,
-            [faculty.email],
-            fail_silently=False,
-        )
-
-        send_notification_faculty(faculty, current_task, days_remaining, time_remaining)
+        try:
+            send_mail(
+                f"Reminder for New Hire Onboarding Task: {current_task.title}",
+                message,
+                settings.EMAIL_HOST_USER,
+                [faculty.email],
+                fail_silently=False,
+            )
+        except Exception as e:
+            pass
 
     return redirect('dashboard:admin_home')
 
@@ -140,13 +143,36 @@ def send_reminder_admin(request, faculty_id, current_task_id, time_remaining):
 
     send_notification_admin(admin, faculty, current_task, time_remaining)
 
-    # send_mail(
-    #     f"Reminder for New Hire Onboarding Task: {current_task.title}",
-    #     message,
-    #     settings.EMAIL_HOST_USER,
-    #     [faculty.email],
-    #     fail_silently=False,
-    # )
+    first_sentence = f"This is a reminder about {faculty.first_name} {faculty.last_name}'s onboarding task: {current_task.title}."
+    second_sentence = f"This task is due on {current_task.deadline.strftime('%B %d, %Y, %I:%M %p')}."
+    third_sentence = f"{time_remaining}"
+    fourth_sentence = "Please have them complete this task as soon as possible."
+    fifth_sentence = "Please have them navigate to the new hire dashboard in the SMSE Onboarding Portal to view the task and complete it."
+
+    message = f"""Hello {admin.first_name} {admin.last_name},
+
+{first_sentence} {second_sentence} {third_sentence} {fourth_sentence} {fifth_sentence}
+
+See below for the task details:
+
+Title: {current_task.title}
+Assigned To: {faculty.first_name} {faculty.last_name}
+Created: {current_task.created_at.strftime('%B %d, %Y, %I:%M %p')}
+Deadline: {current_task.deadline.strftime('%B %d, %Y, %I:%M %p')}
+Prerequisite Task: {current_task.prerequisite_task.title if current_task.prerequisite_task else "None"}
+Remaining Time: Less than 24 hours
+Description: {current_task.description}"""
+
+    try:
+        send_mail(
+            f"Reminder for {faculty.first_name} {faculty.last_name}'s Onboarding Task: {current_task.title}",
+            message,
+            settings.EMAIL_HOST_USER,
+            [admin.email],
+            fail_silently=False,
+        )
+    except Exception as e:
+        pass
 
 def send_notification_admin(admin, faculty, current_task, time_remaining):
     first_sentence = f"This is a reminder about {faculty.first_name} {faculty.last_name}'s onboarding task: {current_task.title}."
