90081523112790a52a3a8b3d7abab58f7183ba61
Long Pham <longpham@sandiego.edu>
2025-04-29 19:16:27 -0700

    Almost finished with admin notifications

--- smse_onboarding_main_project/reminders/urls.py
+++ smse_onboarding_main_project/reminders/urls.py
@@ -5,7 +5,7 @@ app_name = 'reminders'
 
 urlpatterns = [
     path('', views.notifications_page, name='notifications'),
-    path('send-reminder/<int:faculty_id>/<int:current_task_id>/', views.send_reminder, name='send_reminder'),
+    path('send-reminder-faculty/<int:faculty_id>/<int:current_task_id>/', views.send_reminder_faculty, name='send_reminder_faculty'),
     path('mark-as-read/<int:reminder_id>/', views.mark_as_read, name='mark_as_read'),
     path('mark-as-unread/<int:reminder_id>/', views.mark_as_unread, name='mark_as_unread'),
 ]
